81f0627298edec8382e27b65c41d956dd1e6779b
2872zo <yunsg3023@gmail.com>
2019-10-11 17:19:25 +0900

    26장 까지 완료

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Responsive from "../common/Responsive";
 import palette from "../../lib/styles/palette";
 import Button from "../common/Button";
-import SubInfo from "../common/Tags";
+import SubInfo from "../common/SubInfo";
 import Tags from "../common/Tags";
 
 const PostListBlock = styled(Responsive)`
@@ -41,30 +42,45 @@ const PostItemBlock = styled.div`
 	}
 `;
 
-const PostItem = () => {
+const PostItem = ({ post }) => {
+	const { publishedDate, user, tags, title, body, _id } = post;
 	return (
 		<PostItemBlock>
-			<h2>제목</h2>
-			<SubInfo username="username" publishedDate={new Date()} />
-			<Tags tags={["태그1", "태그2", "태그3"]}></Tags>
-			<p>포스트 내용의 일부분..</p>
+			<h2>
+				<Link to={`/@${user.username}/${_id}`}>{title}</Link>
+			</h2>
+			<SubInfo
+				username={user.username}
+				publishedDate={new Date(publishedDate)}
+			/>
+			<Tags tags={tags} />
+			<p>{body}</p>
 		</PostItemBlock>
 	);
 };
 
-const PostList = () => {
+const PostList = ({ posts, loading, error, showWriteButton }) => {
+	// 에러 발생 시
+	if (error) {
+		return <PostListBlock>에러가 발생했습니다.</PostListBlock>;
+	}
+
 	return (
 		<PostListBlock>
 			<WritePostButtonWrapper>
-				<Button cyan to="/write">
-					새 글 작성하기
-				</Button>
+				{showWriteButton && (
+					<Button cyan to="/write">
+						새 글 작성하기
+					</Button>
+				)}
 			</WritePostButtonWrapper>
-			<div>
-				<PostItem />
-				<PostItem />
-				<PostItem />
-			</div>
+			{!loading && posts && (
+				<div>
+					{posts.map(post => (
+						<PostItem post={post} key={post._id} />
+					))}
+				</div>
+			)}
 		</PostListBlock>
 	);
 };
